3be6095ee5826d24f1aec60ddcbea24046d46bf4
SomeJakeGuy <somejakeguy@gmail.com>
2026-08-30 19:36:16 -0400

    V1 of IncludeStrategy

---
+++ include/Sato/IncludeStrategy.hpp
@@ -0,0 +1,32 @@
+// Based on the previous decompilation effort: https://github.com/CoNesTra/zmansion/blob/main/include/Unsorted/IncludeStrategy.h
+
+#ifndef INCLUDE_STRATEGY_HPP
+#define INCLUDE_STRATEGY_HPP
+
+#include <types.h>
+
+#include "Sato/EnemyStrategy.hpp"
+
+/*
+.data has lbl_8035A1D8 as part of this split, maybe EnemyStrategy, IncludeStrategy and a couple others are actually a much bigger TU?
+*/
+
+class IncludeStrategy
+{
+    u8 mStrategyMemory[0x800];
+    EnemyStrategy* mpStrategy;
+    
+public:
+    IncludeStrategy();
+    
+    virtual ~IncludeStrategy();
+    virtual void unknownVirtualFunc() = 0;
+
+    void setStrategy(int);
+    void destroyStrategy();
+
+    void* getBuffer() { return mStrategyMemory; }
+};
+
+
+#endif
---
+++ src/Sato/IncludeStrategy.cpp
@@ -0,0 +1,28 @@
+// Based on the previous decompilation effort: https://github.com/CoNesTra/zmansion/blob/main/src/Unsorted/IncludeStrategy.cpp
+
+#include "Sato/IncludeStrategy.hpp"
+#include "Sato/EnemyStrategy.hpp"
+
+IncludeStrategy::IncludeStrategy() : mpStrategy(nullptr) {
+    destroyStrategy();
+}
+
+IncludeStrategy::~IncludeStrategy() {
+    destroyStrategy();
+}
+
+void IncludeStrategy::setStrategy(int entityIdx) {
+    destroyStrategy();
+    mpStrategy = reinterpret_cast<EnemyStrategy*>(*EnemyStrategy::getInitFunction(entityIdx));
+}
+
+void IncludeStrategy::destroyStrategy() {
+    EnemyStrategy* strategy = mpStrategy;
+
+    if (strategy != nullptr) {
+        // This object was initialized via placement new, so we
+        // can just call its destructor and invalidate its pointer.
+        strategy->~EnemyStrategy();
+        mpStrategy = nullptr;
+    }
+}
