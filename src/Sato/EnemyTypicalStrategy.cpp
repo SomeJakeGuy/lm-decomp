4bc46c8351786cae37dfd026275984aae33127b4
Demorck <57636452+Demorck@users.noreply.github.com>
2026-09-15 18:56:12 +0200

    Explicit comment for EnemyTypicalStrategy::onFishingBegin

--- src/Sato/EnemyTypicalStrategy.cpp
+++ src/Sato/EnemyTypicalStrategy.cpp
@@ -4,7 +4,7 @@ dummy_float_data()
 static float lbl_8035B0F0[8] = { 1.0f, 0.95f, 0.9f, 0.85f, 1.0f, 0.9f, 0.75f, 0.3f };
 
 bool EnemyTypicalStrategy::onFishingBegin(Koga::CharacterEvent* msg) {
-    EnemyStrategy* tsuri; // = fn_800E410C(unk_20, mpZako)
+    EnemyStrategy* tsuri; // = fn_800E410C(unk_20, mpZako->0x808->0x3C)
 
     tsuri->mpZako = mpZako;
     tsuri->mpUserData = mpUserData;
