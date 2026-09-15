1163edbdb578b21cc453ed221f97b90c775d31e0
Demorck <57636452+Demorck@users.noreply.github.com>
2026-09-15 18:53:55 +0200

    Adding EnemyTypicalStrategy

---
+++ src/Sato/EnemyTypicalStrategy.cpp
@@ -0,0 +1,16 @@
+#include "Sato/EnemyTypicalStrategy.hpp"
+
+dummy_float_data()
+static float lbl_8035B0F0[8] = { 1.0f, 0.95f, 0.9f, 0.85f, 1.0f, 0.9f, 0.75f, 0.3f };
+
+bool EnemyTypicalStrategy::onFishingBegin(Koga::CharacterEvent* msg) {
+    EnemyStrategy* tsuri; // = fn_800E410C(unk_20, mpZako)
+
+    tsuri->mpZako = mpZako;
+    tsuri->mpUserData = mpUserData;
+    tsuri->setNextState(0);
+
+    setTsuriStrategy(tsuri);
+
+    return true;
+}
