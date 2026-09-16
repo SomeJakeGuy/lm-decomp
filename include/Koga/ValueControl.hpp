c333dc302306821099fbda7b7f98e9a51970fcfb
Demorck <57636452+Demorck@users.noreply.github.com>
2026-09-16 17:19:00 +0200

    Fixing define ValueControl hpp

--- include/Koga/ValueControl.hpp
+++ include/Koga/ValueControl.hpp
@@ -1,11 +1,13 @@
-#ifndef ENEMY_GEN_MANAGER_HPP
-#define ENEMY_GEN_MANAGER_HPP
+#ifndef KOGA_VALUE_CONTROL_HPP
+#define KOGA_VALUE_CONTROL_HPP
 
 
 namespace Koga {
 
     template<typename T>
     class ValueControl {
+        public:
+            /* 0x08 */ virtual void  vt_08() = 0;
     };
 }
 
