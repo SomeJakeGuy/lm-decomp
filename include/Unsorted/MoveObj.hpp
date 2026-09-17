003bc9561ef681a307b04eb40646655b67887458
Demorck <57636452+Demorck@users.noreply.github.com>
2026-09-17 23:29:16 +0200

    Starting MoveObj

--- include/Unsorted/MoveObj.hpp
+++ include/Unsorted/MoveObj.hpp
@@ -1,9 +1,12 @@
 #ifndef MOVE_OBJ_H_
 #define MOVE_OBJ_H_
 
+#include "JSystem/JAudio/JAInterface/JAIAnimation.hpp"
+#include "JSystem/JGeometry/JGVec3.hpp"
 #include <types.h>
 #include <dolphin/mtx.h>
 
+// 0xe3, 0xe7
 class MoveObj {
 public:
     MoveObj();
@@ -19,11 +22,32 @@ public:
     bool fn_800E7650();
 
     inline s32 get_3C() const { return _3C; }
+
+protected:
+    void fn_80067CB0(double var1, double var2, double var3);
+
 public:
     /* 0x004 */ u8 _0[0x34];
     /* 0x038 */ s32 _38;
     /* 0x03C */ s32 _3C;
-    /* 0x040 */ u8 _40[0x788 - 0x40];
+    /* 0x040 */ s32 _40;
+
+    
+    /* 0x044 */ JGeometry::TVec3<f32> _vec44;
+    /* 0x050 */ JGeometry::TVec3<f32> _vec50;
+    /* 0x05c */ JGeometry::TVec3<f32> _vec5c;
+
+    /* 0x068 - 0x53c */ u8 _68[0xb4 - 0x68];
+
+    /* 0x0b4 */ float _b4;
+    
+    /* 0x0b8 - 0x53c */ u8 _b8[0x53c - 0xb8];
+
+    /* 0x53c */ JAIAnimeSound mAnimeSound;
+
+    /* 0x040 */ u8 _54c[0x788 - 0x5d4];
+
+    /* 0x788 */ u8 _size; // TEMP ! USED JUST FOR MY CALCULATION FOR THE SIZE TO NOT DESTROY EVERYTHING
 };
 
 #endif
--- src/Unsorted/MoveObj.cpp
+++ src/Unsorted/MoveObj.cpp
@@ -1,6 +1,24 @@
 #include "Unsorted/MoveObj.hpp"
+#include "macros.h"
 
+dummy_float_data();
+enemies_float_data();
 
 MoveObj::MoveObj() {
+    _size = true;
+}
 
+MoveObj::~MoveObj() {
+    
+}
+
+void MoveObj::fn_80067CB0(double var1, double var2, double var3) {
+    _vec44.set(var1, var2, var3);
+    _vec5c.set(_vec44);
+    _vec50.set(_vec5c);
+
+    Vec vec;
+    vec.x = _vec44.x;
+    vec.y = _vec44.y;
+    vec.z = _vec44.z;
 }
