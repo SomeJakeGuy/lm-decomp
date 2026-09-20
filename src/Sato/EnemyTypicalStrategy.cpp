3ad8293b79ed5ff915721f07410b888e68f23126
Demorck <57636452+Demorck@users.noreply.github.com>
2026-09-20 03:01:16 +0200

    Changing EnemyTypicalStrategy float by the macro

--- src/Sato/EnemyTypicalStrategy.cpp
+++ src/Sato/EnemyTypicalStrategy.cpp
@@ -1,7 +1,8 @@
 #include "Sato/EnemyTypicalStrategy.hpp"
+#include "macros.h"
 
 dummy_float_data()
-static float lbl_8035B0F0[8] = { 1.0f, 0.95f, 0.9f, 0.85f, 1.0f, 0.9f, 0.75f, 0.3f };
+enemies_float_data()
 
 bool EnemyTypicalStrategy::onFishingBegin(Koga::CharacterEvent* msg) {
     EnemyStrategy* tsuri; // = fn_800E410C(unk_20, mpZako->0x808->0x3C)
