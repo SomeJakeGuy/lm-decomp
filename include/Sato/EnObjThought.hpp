acbd6a275a46c30509da367d4dfc4a9d04fab1d5
SomeJakeGuy <somejakeguy@gmail.com>
2026-09-22 08:00:58 -0400

    Initial EnObjThought split + symbols

--- include/Sato/EnObjThought.hpp
+++ include/Sato/EnObjThought.hpp
@@ -1,13 +1,12 @@
 #ifndef EN_OBJ_THOUGHT_HPP
 #define EN_OBJ_THOUGHT_HPP
 
-#include "EnThought.hpp"
+#include "Sato/EnThought.hpp"
 
 class EnObjThought : public EnThought {
     EnObjThought();
     virtual ~EnObjThought();
     virtual void vt_0C(); // Did not check args/return type
-    virtual void vt_10(); // Did not check args/return type
 };
 
 #endif
